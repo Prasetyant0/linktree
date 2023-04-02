a8bbcc2e9e5974058a62e0b1f7931c182aa38b69
Prasetyanto <ihot849@gmail.com>
2023-04-02 19:49:39 +0700

    initial project linktree

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React from "react";
 import './App.css';
 import Linktree from './components/linktree';
-import profilePicture from './images/IMG_20230320_212729.jpg';
+import Footer from "./components/footer";
+import profilePicture from './images/bowo.png';
+import waIcon from './images/wa.png';
+import fbIcon from './images/fb.png';
+import pWIcon from './images/laptop.png';
+import gitIcon from './images/github.png';
 
 
 function App() {
   const links = [
-    { name: 'Facebook', url: 'https://www.facebook.com/yourusername' },
-    { name: 'Twitter', url: 'https://www.twitter.com/yourusername' },
-    { name: 'Instagram', url: 'https://www.instagram.com/prasty_08/' },
-    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/yourusername' },
-    { name: 'GitHub', url: 'https://www.github.com/Prasetyant0/' }
+    { img: waIcon, name: 'WhatsApp', url: 'https://api.whatsapp.com/send?phone=6285861526198&text=Hallo%20mas' },
+    { img: pWIcon, name: 'Personal Website', url: 'http://bowo-profile.epizy.com/' },
+    { img: fbIcon, name: 'Facebook', url: 'https://www.facebook.com/prasty.b0wo' },
+    { img: gitIcon, name: 'GitHub', url: 'https://www.github.com/Prasetyant0/' }
   ];
 
   return (
-    <div className="App">
-      <div className="profile">
+  <div className="app">
+    <div className="profile">
+      <div className="picture-container">
         <img src={profilePicture} alt="Profile" className="profile-picture" />
-        <h2>Prasetyanto Tri Prabowo</h2>
       </div>
-      <h1 className="title">My Social Media Link</h1>
-      <Linktree links={links} />
+      <h2 className="name">@Prasetyanto</h2>
     </div>
-  );
+    <h1 className="title">My Social Media Link</h1>
+    <div className="link-container">
+      <div className="link-images">
+        {links.map(link => (
+          <div key={link.name}>
+            <a href={link.url} target="_blank" rel="noopener noreferrer" className="link-item">
+              <img className="imgIcon" src={link.img} alt={link.name} />
+              <div className="link-name">
+                <p>{link.name}</p>
+              </div>
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+    <Footer />
+  </div>
+);
+
+
+
 }
 
 export default App;
---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>Make with &hearts; by Prasetyanto Tri Prabowo</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
